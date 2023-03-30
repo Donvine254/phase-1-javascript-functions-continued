5f1c88b7ef473ba80f1a22863d03d6c10f688d9d
donvine mugendi <donvinemugendi@gmail.com>
2023-03-30 20:34:03 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  if (activity) {
+    return `This Saturday, I want to ${activity}!`;
+  } else {
+    return `This Saturday, I want to ${activity}!`;
+  }
+}
+function mondayWork(activity = "go to the office") {
+  if (activity) {
+    return `This Monday, I will ${activity}.`;
+  } else {
+    return `This Monday, I will ${activity}.`;
+  }
+}
+function wrapAdjective(flair = "*") {
+  return function (adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
